3b597a70cf22473b365f61c0eaffca48e57082a2
fabimass <massotto.fabian@gmail.com>
2023-04-20 13:37:06 -0300

    leaving only repo and commit models in admin page

--- submissions/admin.py
+++ submissions/admin.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
-from .models import User, Repo, Commit, Branch, Pull
+from .models import Repo, Commit
 
-class BranchAdmin(admin.ModelAdmin):
-    filter_horizontal = ("commits",)
-
-admin.site.register(User)
 admin.site.register(Repo)
 admin.site.register(Commit)
-admin.site.register(Branch, BranchAdmin)
-admin.site.register(Pull)
